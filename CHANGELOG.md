547a262ee8fc0e7354bde5dd176070f6bd28c7d1
Vicky Laurijssen <vicky.laurijssen@phpro.be>
2023-08-08 12:52:07 +0200

    Added release 1.0.0

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## [1.0.0] - 2023-08-04
+### Added
+- Configuration which disallows removing shopping lists
+- Configuration which adds shopping list info to order grid in customer account
---
+++ Twenty5CaratOrdersByShoppingListBundle.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Twenty5Carat\OrdersByShoppingListBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Twenty5CaratOrdersByShoppingListBundle extends Bundle
+{
+}
